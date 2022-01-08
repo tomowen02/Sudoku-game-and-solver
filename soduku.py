2c1dfad51b161b7151fc5f26b57d980104fb1c44
Tom Owen <tom@tomiowen.com>
2022-01-08 16:36:40 +0000

    Initial commit

---
+++ soduku.py
@@ -0,0 +1,55 @@
+board = [
+    [], # Bottom section (0)
+    [], # Middle section (1)
+    [] # Top section (2)
+]
+
+def print_board(board):
+    print(f'''
+    {print_row(board[0][0])}
+    {print_row(board[0][1])}
+    {print_row(board[0][2])}
+    ------+-------+------
+    {print_row(board[0][0])}
+    {print_row(board[0][1])}
+    {print_row(board[0][2])}
+    ------+-------+------
+    {print_row(board[0][0])}
+    {print_row(board[0][1])}
+    {print_row(board[0][2])}
+    ''')
+
+def print_section(cage):
+    row1 = cage[0]
+    row2 = cage[1]
+    row3 = cage[2]
+
+    print(f'''    {print_row(row1)}
+    {print_row(row2)}
+    {print_row(row3)}''')
+
+def print_row(row):
+    return (f"{row[0][0]} {row[0][1]} {row[0][2]} | {row[1][0]} {row[1][1]} {row[1][2]} | {row[2][0]} {row[2][1]} {row[2][2]}")
+
+
+x1 = [
+    [[1], [2], [3]], [[4], [5], [6]], [[7], [8], [9]]
+]
+
+x2 = [
+    [[2], [2], [3]], [[4], [5], [6]], [[7], [8], [9]]
+]
+
+x3 = [
+    [[3], [2], [3]], [[4], [5], [6]], [[7], [8], [9]]
+]
+
+y1 = [x1, x2, x3]
+y2 = [x1, x2, x3]
+y3 = [x1, x2, x3]
+
+z = [y1, y2, y3]
+
+#print_board(z)
+
+print(x1[0][0])
